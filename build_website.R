ececb2c8d7ae674062e867a2583d2e383782cce8
Lukas Gaats <57689809+lgaats@users.noreply.github.com>
2020-12-03 18:03:38 +0100

    Password-Implementation

--- build_website.R
+++ build_website.R
@@ -15,7 +15,7 @@ rmarkdown::render_site(encoding = 'UTF-8')
 ###### THIS WILL ONLY WORK IF YOU HAVE STATICRYPT INSTALLED VIA NPM ######
 ##########################################################################
 
-system( paste0("staticrypt ./docs/journal.html ", Sys.getenv("LAB_KEY")) )
+system( paste0("staticrypt ./docs/journal.html ", LAB_KEY) )
 
 # Remove and rename resulting files
 file.remove("./docs/journal.html")
